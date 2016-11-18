1b102834733c0f4bea2106851f3a7912fec10cc1
carrieh457 <ch646@cornell.edu>
2016-11-18 15:54:01 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Apple Harvest</title>
+		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+	</head>
+	
+	<body>
+		<?php include 'includes/nav_bar.php';?>
+	  
+		<div id="hero">
+			<img src="images/apples-1285236_1920.jpg" alt="Banner">
+		</div>
+	  
+		<div id="container">
+			<h2>About our Site</h2>
+      
+      <!-- Media Credit : Andrew Halpern Flickr -->
+		<footer>
+		</footer>
+  </body>
+</html>
