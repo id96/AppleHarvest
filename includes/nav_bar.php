63520ac5031bc7fa9b3bf8ff50641bd46c875b66
carrieh457 <ch646@cornell.edu>
2016-11-18 15:53:25 -0500

    Add files via upload

---
+++ includes/nav_bar.php
@@ -0,0 +1,15 @@
+<div id="top">
+	<div id="banner">
+		<img src="images/Apple-Basket-PNG-image1.png">
+		<h2>Ithaca Apple Harvest Festival</h2>
+	</div>
+	
+	<div id="nav_bar">
+		<ul>
+			<li> <a href="index.php">Home</a></li>
+			<li><a href="activities.php">Activities</a></li>
+			<li><a href="directions.php">Directions</a></li>
+		</ul>
+	</div><!-- end of nav_bar div -->
+			
+</div><!-- end of top div -->
