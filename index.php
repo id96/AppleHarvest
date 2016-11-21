590580b75bdcdfcc84959f9515e05279e35dc15e
Evan Zhao <etz3@cornell.edu>
2016-11-21 16:51:00 -0500

    fixed

--- index.php
+++ index.php
@@ -6,17 +6,28 @@
 		<link rel="stylesheet" type="text/css" href="styles/styles.css">
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 	</head>
-	
+
 	<body>
 		<?php include 'includes/nav_bar.php';?>
-	  
+
 		<div id="hero">
 			<img src="images/apples-1285236_1920.jpg" alt="Banner">
 		</div>
-	  
-		<div id="container">
+
+		<div id="content">
 			<h2>About our Site</h2>
-      
+			<!-- <div id="button_container">
+          <button id="prev" onclick="movePrevious()">Previous</button>
+          <button id="next" onclick="moveNext()">Next</button>
+      </div> <!-- end of button_container div --> -->
+
+			<!-- <div id="image_container">
+				<img id="display" src="images/Banner.jpg">
+			<!-- Media Credit : Andrew Halpern Flickr -->
+			 <!-- Media Credit : Hannah MacLean Flickr -->
+
+		</div> -->
+
       <!-- Media Credit : Andrew Halpern Flickr -->
 		<footer>
 		</footer>
