59f55b432cc10ba9be8dab05dcf6470808ec8ae9
Evan Zhao <etz3@cornell.edu>
2016-11-21 13:47:40 -0500

    footer

--- directions.php
+++ directions.php
@@ -3,24 +3,16 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Apple Harvest</title>
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 	</head>
 
 	<body>
 		<?php include 'includes/nav_bar.php';?>
 
-		<div id="hero">
-			<img src="images/apples-1285236_1920.jpg" alt="Banner">
-		</div>
-
-		<div class="content">
-			<h2>About our Site</h2>
-		</div>
-
       <!-- Media Credit : Andrew Halpern Flickr -->
 		<footer>
-			<?php include 'includes/footer.php'; ?>
+      <?php include 'includes/footer.php'; ?>
 		</footer>
   </body>
 </html>
---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div id="footer">
+  <div id="use_bar">
+    <ul class="soc">
+        <li><a class="soc-twitter" href="http://google.com"></a></li>
+        <li><a class="soc-facebook" href="#"></a></li>
+        <li><a class="soc-flickr" href="#"></a></li>
+        <li><a class="soc-youtube" href="#"></a></li>
+        <li><a class="soc-mail soc-icon-last" href="#"></a></li>
+    </ul>
+    <p class="footStyle">Ithaca Apple Harvest Festival</p>
+  </div>
+
+  <div id="credits">
+    <p>Made with &#9829; in Ithaca</p>
+  </div>
+
+</div><!-- end of top div -->
