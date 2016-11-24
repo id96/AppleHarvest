c13f779c3aec7ada2fd34c92311533ab0bd9ccbb
carrieh457 <ch646@cornell.edu>
2016-11-23 21:58:01 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,34 +3,65 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Apple Harvest</title>
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 		<link rel="stylesheet" type="text/css" href="styles/styles.css">
 	</head>
 
 	<body>
 		<?php include 'includes/nav_bar.php';?>
 
-		<div id="hero">
-			<img src="images/apples-1285236_1920.jpg" alt="Banner">
-		</div>
+		<div id="event_container">
+				<ul>
+					<li>
+					<div id="event_info">
+						<div id="info">
+							<h3>September 30th to </h3>
+							<h3>October 2nd</h3>
+						</div>
+						<div id="icon">
+							<img src="images/date_icon.png">
+						</div>
+					</div><!-- end of _event_date div -->
+					</li>
 
+					<li>
+					<div id="event_info">
+						<div id="info">
+							<h3>Ithaca Commons</h3>
+							<h4>dlfajldsajflkjds</h4>
+						</div>
+						<div id="icon">
+							<img src="images/date_icon.png">
+						</div>
+					</div><!-- end of _event_date div -->
+					</li>
+
+					<li>
+					<div id="event_info">
+						<div id="info">
+							<h3>Ithaca Commons</h3>
+							<h4>dlfajldsajflkjds</h4>
+						</div>
+						<div id="icon">
+							<img src="images/date_icon.png">
+						</div>
+					</div><!-- end of _event_date div -->
+					</li>
+				</ul>
+			</div><!-- end of _event_container div -->
+		
 		<div id="content">
-			<h2>About our Site</h2>
-			
+	
+			<div id="spacer"></div>
+			<div id="description">
+				<h1>Ithaca's premier event of the Fall, Apple Harvest Festival is a three day celebration of food, fun, and apples!</h1>
+			</div>
+		
 			<iframe id="youtube" width="560" height="315" src="https://www.youtube.com/embed/s9zZR99jfkU" frameborder="0" allowfullscreen></iframe>
-			<!-- <div id="button_container">
-          <button id="prev" onclick="movePrevious()">Previous</button>
-          <button id="next" onclick="moveNext()">Next</button>
-      </div> -->
+			
+		</div> <!-- end of content div -->
 
-			<!-- <div id="image_container">
-				<img id="display" src="images/Banner.jpg">
-			<!-- Media Credit : Andrew Halpern Flickr -->
-			 <!-- Media Credit : Hannah MacLean Flickr -->
-
-		</div> -->
-
-      <!-- Media Credit : Andrew Halpern Flickr -->
 		<footer>
 			<?php include 'includes/footer.php'; ?>
 		</footer>
