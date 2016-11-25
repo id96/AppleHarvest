0a0dd923050a78011501c0f3102f184d0db601f3
JialuJialu <baojialuhz@gmail.com>
2016-11-24 19:21:16 -0500

    direction_1Version

--- directions.php
+++ directions.php
@@ -9,8 +9,16 @@
 
 	<body>
 		<?php include 'includes/nav_bar.php';?>
+		
 
 		<div class="content">
+			<div id="directionimg">
+				<img src="images/AppleMap2016.jpg">
+				<!-- Media Credit : Downtown Ithaca Website -->
+			</div>
+			<div id="googlemap">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2944.443033500038!2d-76.49993280325015!3d42.439588879181684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d0819c8632d9e5%3A0x7e9b07ab2aa65eb6!2sIthaca+Commons!5e0!3m2!1sen!2sus!4v1480029466179" width=80% height="450" frameborder="60" style="border:0" allowfullscreen></iframe>
+			</div>
 		</div>
 
       <!-- Media Credit : Andrew Halpern Flickr -->
