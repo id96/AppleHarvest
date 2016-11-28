1702e8dc490686ebbb58c4c4392f6b233fae011d
Evan Zhao <etz3@cornell.edu>
2016-11-28 12:23:06 -0500

    fixed bad merge, redid nav bar spacing, and footer spacing

--- includes/nav_bar.php
+++ includes/nav_bar.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 0a0dd923050a78011501c0f3102f184d0db601f3
 <div id="nav_bar">
 	<ul>
 		<li><a href="index.php">Home</a></li>
@@ -9,17 +5,8 @@
 		<li><a href="directions.php">Directions</a></li>
 	</ul>
 </div><!-- end of nav_bar div -->
-<<<<<<< HEAD
-		
-<div id="hero">
-	<img src="images/apples-1285236_1920.jpg" alt="Banner">
-=======
 
-		
 <div id="hero">
 	<img src="images/apple-banner.jpg" alt="Banner">
 	<!-- Media Credit : Couleur pixabay -->
->>>>>>> 0a0dd923050a78011501c0f3102f184d0db601f3
 </div>
-
-
