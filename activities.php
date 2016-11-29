fd5eb22fc8bdeb22f97ab9b0d672acbf1caef819
Isabelle De Brabanter <isabelledebrabanter@dhcp-clara-3686.eduroam.cornell.edu>
2016-11-28 21:36:01 -0500

    activities page update

--- activities.php
+++ activities.php
@@ -11,15 +11,37 @@
 	<body>
 		<?php include 'includes/nav_bar.php';?>
 
-		<div class="content">
-		<h2>Sample Vendors</h2>
-			<ul>
-				<li>Laughing Goat Fiber Farm</li>
-				<li>Wineries</li>
-				<li>Artisans with Crafts</li>
-			</ul>
+		<h1 id='title'>What is there to do at Apple Fest?</h1>
+
+		<div class="section">
+			<img class="section_image" src="images/eatlikenoone.jpg"/>
+			<div class="section_content right">
+				<h1>Food Vendors</h1>
+				<p>List of Food: Taste of Thai, Food Trucks</p>
+			</div>
+		</div>
+	
+		<hr>
+	
+		<div class="section">
+			<div class="section_content left just">
+				<h1>Crafts</h1>
+				<p>Face Painting</p>
+			</div>
+			<img class="section_image" src="images/facepainting.jpg" />
+			<!-- Media Credit: Photo by Alisha Tamarchenko -->
+	
+		</div>
 		
-		
+		<hr>
+
+		<div class="section">
+			<img class="section_image" src="images/fallcreekbrass.jpg"/>
+			<!-- Media Credit: Photo by Alisha Tamarchenko-->
+			<div class="section_content right">
+				<h1>Music</h1>
+				<p>Live Performances and Singing</p>
+			</div>
 		</div>
 		
       
