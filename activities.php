c0fdfaf832ef2c46db7f010224cb7100c17bc2be
Isabelle De Brabanter <isabelledebrabanter@dhcp-clara-3686.eduroam.cornell.edu>
2016-11-28 21:44:09 -0500

    image resize

--- activities.php
+++ activities.php
@@ -14,10 +14,11 @@
 		<h1 id='title'>What is there to do at Apple Fest?</h1>
 
 		<div class="section">
-			<img class="section_image" src="images/eatlikenoone.jpg"/>
+			<img class="activity_image" src="images/eatlikenoone.jpg"/>
 			<div class="section_content right">
 				<h1>Food Vendors</h1>
 				<p>List of Food: Taste of Thai, Food Trucks</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
 	
@@ -27,8 +28,9 @@
 			<div class="section_content left just">
 				<h1>Crafts</h1>
 				<p>Face Painting</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
-			<img class="section_image" src="images/facepainting.jpg" />
+			<img class="activity_image" src="images/facepainting.jpg" />
 			<!-- Media Credit: Photo by Alisha Tamarchenko -->
 	
 		</div>
@@ -36,11 +38,12 @@
 		<hr>
 
 		<div class="section">
-			<img class="section_image" src="images/fallcreekbrass.jpg"/>
+			<img class="activity_image" src="images/fallcreekbrass.jpg"/>
 			<!-- Media Credit: Photo by Alisha Tamarchenko-->
 			<div class="section_content right">
 				<h1>Music</h1>
 				<p>Live Performances and Singing</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
 		
