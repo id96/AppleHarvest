ef7bd34b615fe27dd53b0a688c58876db5316109
carrieh457 <ch646@cornell.edu>
2016-12-06 00:21:25 -0500

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Apple Harvest</title>
+		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+	</head>
+
+	<body>
+		<?php include 'includes/nav_bar.php';?>
+
+		<h1 id='title'>Photo Gallery</h1>
+
+		<footer>
+			<?php include 'includes/footer.php'; ?>
+		</footer>
+  </body>
+</html>
