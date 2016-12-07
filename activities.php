0605e3b8e32a006f8f71bfb123a8488aec6a6c28
Evan Zhao <etz3@cornell.edu>
2016-12-06 21:55:25 -0500

    columns

--- activities.php
+++ activities.php
@@ -21,11 +21,13 @@
 
 		<div class="section-background">
 			<div class="section">
-				<div class="left"><img class="activity_image" src="images/eatlikenoone.jpg" alt='Eat Like No One is Watching'/></div>
-				<div class="section_content">
-					<h1>Food Vendors</h1>
-					<p>Over 100 vendors will be selling unique apple varieties, hot apple cider donuts, kettle corn, local farm fresh baked goods and a multitude of around the world specialties. Local farmers are on site selling everything from tasty apple beverages to local pumpkins and of course, apples, apples and more apples. Wineries and cideries have regional beverages available for tasting and purchase.</p>
-					<!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
+				<div class="col"><img class="activity_image" src="images/eatlikenoone.jpg" alt='Eat Like No One is Watching'/></div>
+				<div class="col">
+					<div class="section_content">
+						<h1>Food Vendors</h1>
+						<p>Over 100 vendors will be selling unique apple varieties, hot apple cider donuts, kettle corn, local farm fresh baked goods and a multitude of around the world specialties. Local farmers are on site selling everything from tasty apple beverages to local pumpkins and of course, apples, apples and more apples. Wineries and cideries have regional beverages available for tasting and purchase.</p>
+						<!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
+					</div>
 				</div>
 			</div>
 		</div>
@@ -33,33 +35,37 @@
 		<hr>
 
 		<div class="section">
-			<div class="left"> <img class="activity_image" src="images/facepainting.jpg" alt='Face Painting'/></div>
-			<div class="section_content">
-				<h1>Crafts</h1>
-				<p>Looking for quality goods, visit 60 different handcrafting artisans along Cayuga Street at the fall craft show which includes jewelry, ceramics, home decor, body care, specialty gifts, unique sculpture, clothing and so much more.</p>
-				<!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
-				<p>In addition, we also have face painting, and kids can visit with the goats and learn how fiber is made with Laughing Goat Fiber Farm.</p>
+			<div class="col"> <img class="activity_image" src="images/facepainting.jpg" alt='Face Painting'/></div>
+			<div class="col">
+				<div class="section_content">
+					<h1>Crafts</h1>
+					<p>Looking for quality goods, visit 60 different handcrafting artisans along Cayuga Street at the fall craft show which includes jewelry, ceramics, home decor, body care, specialty gifts, unique sculpture, clothing and so much more.</p>
+					<!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
+					<p>In addition, we also have face painting, and kids can visit with the goats and learn how fiber is made with Laughing Goat Fiber Farm.</p>
+				</div>
 			</div>
 			<!-- Media Credit: Photo by Alisha Tamarchenko -->
 		</div>
 
 		<div class="section-background">
 			<div class="section">
-				<div class="left"><img class="activity_image_special" src="images/fallcreekbrass.jpg" alt="activity image"/></div>
+				<div class="col"><img class="activity_image" src="images/fallcreekbrass.jpg" alt="activity image"/></div>
 				<!-- Media Credit: Photo by Alisha Tamarchenko-->
-				<div class="section_content">
-					<h1>Performance Schedule</h1>
-					<p class='day'>Friday:</p>
-					<p>Hilby The Skinny German Juggle Boy 12:30 and 4:45pm</p>
-					<p class='day'>Saturday:</p>
-					<p>12pm- Zajal the Sugarplum Fairy</p>
-					<p>1pm- The Next Jenneration</p>
-					<p>3pm- The Strong Woman Show</p>
-					<p>4:30- IC Circus</p>
-					<p class='day'>Sunday:</p>
-					<p>12:30pm Matan Presberg - Juggler</p>
-					<p>3pm- Hula Hut Polynesian Dance</p>
-				</div> <!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust# -->
+				<div class="col">
+					<div class="section_content">
+						<h1>Performance Schedule</h1>
+						<p class='day'>Friday:</p>
+						<p>Hilby The Skinny German Juggle Boy 12:30 and 4:45pm</p>
+						<p class='day'>Saturday:</p>
+						<p>12pm- Zajal the Sugarplum Fairy</p>
+						<p>1pm- The Next Jenneration</p>
+						<p>3pm- The Strong Woman Show</p>
+						<p>4:30- IC Circus</p>
+						<p class='day'>Sunday:</p>
+						<p>12:30pm Matan Presberg - Juggler</p>
+						<p>3pm- Hula Hut Polynesian Dance</p>
+					</div> <!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust# -->
+				</div>
 			</div>
 		</div>
 
