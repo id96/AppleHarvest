ea24830a2351cb03fb900d4be5b094874a9944a8
Isabelle De Brabanter <isabelledebrabanter@dhcp-gs-795.eduroam.cornell.edu>
2016-12-06 22:07:16 -0500

    responsive on activity page

--- activities.php
+++ activities.php
@@ -49,21 +49,21 @@
 
 		<div class="section-background">
 			<div class="section">
-				<div class="col"><img class="activity_image" src="images/fallcreekbrass.jpg" alt="activity image"/></div>
+				<div class="col"><img class="activity_imageMUSIC" src="images/fallcreekbrass.jpg" alt="activity image"/></div>
 				<!-- Media Credit: Photo by Alisha Tamarchenko-->
 				<div class="col">
 					<div class="section_content">
 						<h1>Performance Schedule</h1>
 						<p class='day'>Friday:</p>
-						<p>Hilby The Skinny German Juggle Boy 12:30 and 4:45pm</p>
+						<p>12:30 and 4:45pm - Hilby The Skinny German Juggle Boy </p>
 						<p class='day'>Saturday:</p>
-						<p>12pm- Zajal the Sugarplum Fairy</p>
-						<p>1pm- The Next Jenneration</p>
-						<p>3pm- The Strong Woman Show</p>
-						<p>4:30- IC Circus</p>
+						<p>12pm - Zajal the Sugarplum Fairy</p>
+						<p>1pm - The Next Jenneration</p>
+						<p>3pm - The Strong Woman Show</p>
+						<p>4:30pm - IC Circus</p>
 						<p class='day'>Sunday:</p>
-						<p>12:30pm Matan Presberg - Juggler</p>
-						<p>3pm- Hula Hut Polynesian Dance</p>
+						<p>12:30pm - Matan Presberg Juggler</p>
+						<p>3pm - Hula Hut Polynesian Dance</p>
 					</div> <!-- credit: http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust# -->
 				</div>
 			</div>
