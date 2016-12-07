8728a73fd5c7ad609c3f411f2f659a8c6e285b64
JialuJialu <baojialuhz@gmail.com>
2016-12-06 20:51:36 -0500

    gallery and image

--- js/main.js
+++ js/main.js
@@ -5,41 +5,37 @@
 */
 //just try with the images we have right now and two images I found on flickr,
 //would replace and resize the images later
-var pix = ["apples-1285236_1920", "hannah_applefest"];
+var pix = ["IV-AppleFest001 (Photo by Hannah MacLean)",
+"IV-AppleFest003 (Photo by Alisha Tamarchenko)", "IV-AppleFest004（Photo by Alisha Tamarchenko",
+"IV-AppleFest026 (Photo by Alisha Tamarchenko)", "IV-AppleFest017 (Photo by Alex Anderson)", 
+"IV-AppleFest009 (Photo by Alisha Tamarchenko)", "IV-AppleFest010 (Photo by Alisha Tamarchenko)"
+];
 var index = 0;
-var main_image = document.getElementById("enlarge");
-//var change_name = "routemap";
+var main_image = document.getElementById("slide_display");
 var roll;
-
-var slideIndex = 1;
-
-function calendar() {
-    if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-    if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-    s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-    s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-    var h = d[g]('body')[0];h.appendChild(s); }}();
 
 //following codes adapted from http://addtocalendar.com/#tab_widget_style-->
 
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    //var x = "apples-1285236_1920", "hannah_applefest"];
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
+// functions
+function movePrevious() {
+    if (index <= 0) {
+        index = pix.length-1;
+    } else {
+        index -= 1;
     }
-    x[slideIndex-1].style.display = "block"; 
+    var imagename = 'images/' + pix[index] + '.jpg'
+    document.getElementById('slide_display').src=imagename;
 }
-//adapt the code from w3 school
+
+function moveNext() {
+    if (index < pix.length-1) {
+        index += 1;
+    } else {
+        index = 0;
+    }
+    var imagename = 'images/' + pix[index] + '.jpg'
+    document.getElementById('slide_display').src=imagename;
+}
 
 
 function swapImg() {
